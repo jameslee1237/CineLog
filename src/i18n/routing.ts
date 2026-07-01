c84fedac22ff916433ca6bb828ef3fc157353372
jameslee1237 <43644092+jameslee1237@users.noreply.github.com>
2026-07-02 08:20:45 +0900

    feat: add next-intl routing/navigation/request config

---
+++ src/i18n/routing.ts
@@ -0,0 +1,8 @@
+import { defineRouting } from 'next-intl/routing';
+import { DEFAULT_LOCALE, LOCALES } from './locales';
+
+export const routing = defineRouting({
+  locales: LOCALES,
+  defaultLocale: DEFAULT_LOCALE,
+  localePrefix: 'always',
+});
